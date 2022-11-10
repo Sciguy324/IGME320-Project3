d6175055ad75a07cc0022200ce871611078b228d
Sciguy324 <webmeyer@yahoo.com>
2022-11-10 11:48:26 -0500

    Started adding spawn manager

--- Project3/Assets/Scripts/Enemy.cs
+++ Project3/Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : GenericEntity
 {
     public int attackDamage;
+    public GenericEntity targetEntity;
+    public SpawnManager spawnManager;
 
     // Start is called before the first frame update
     void Start()
