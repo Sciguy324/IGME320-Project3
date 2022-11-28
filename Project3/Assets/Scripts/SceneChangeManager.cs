1a6dc73a4b6454c2aa3f909b2a67b0cf71440a2d
Tyler Garcia <tmg7946@rit.edu>
2022-11-28 17:30:14 -0500

    Fixed merge conflicts and sucessfully implemented everything fior onboarding

--- Project3/Assets/Scripts/SceneChangeManager.cs
+++ Project3/Assets/Scripts/SceneChangeManager.cs
@@ -9,6 +9,7 @@ public class SceneChangeManager : MonoBehaviour
     public GameObject startScreen;
     public GameObject onboardScreen;
     public GameObject creditsScreen;
+    public Texture2D targetCursorTexture;
 
     public void GoToCredits()
     {
@@ -16,21 +17,19 @@ public class SceneChangeManager : MonoBehaviour
         onboardScreen.SetActive(false);
         startScreen.SetActive(false);
         Debug.Log("Game is going to credits screen");
-    public Texture2D targetCursorTexture;
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
 
     void Start()
     {
         // For when we're not starting on the "start" menu
+        /*
         if (SceneManager.GetActiveScene().name == "SampleScene")
         {
             Cursor.SetCursor(targetCursorTexture, Vector2.zero, CursorMode.Auto);
         }
-    }
-
-    public void GoToCredits()
-    {
-        SceneManager.LoadScene("Credits");
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        */
     }
 
     public void GoToHowToPlay()
@@ -39,7 +38,6 @@ public class SceneChangeManager : MonoBehaviour
         onboardScreen.SetActive(true);
         startScreen.SetActive(false);
         Debug.Log("Game is going to onboarding screen");
-        SceneManager.LoadScene("HowToPlay");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
@@ -53,7 +51,6 @@ public class SceneChangeManager : MonoBehaviour
         // Resume the game
         Time.timeScale = 1;
         Debug.Log("Game is starting");
-        SceneManager.LoadScene("SampleScene");
         Cursor.SetCursor(targetCursorTexture, Vector2.zero, CursorMode.Auto);
     }
 
@@ -63,7 +60,6 @@ public class SceneChangeManager : MonoBehaviour
         onboardScreen.SetActive(false);
         startScreen.SetActive(true);
         Debug.Log("Game is going to start screen");
-        SceneManager.LoadScene("StartMenu");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
