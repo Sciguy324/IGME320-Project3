336c4e9717e1c19152f4ea773d241fe5956e0824
Tyler Garcia <tmg7946@rit.edu>
2022-11-28 17:18:56 -0500

    Added onboarding completely

--- Project3/Assets/Scripts/GameManager.cs
+++ Project3/Assets/Scripts/GameManager.cs
@@ -53,6 +53,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // STOP TIME UNTIL YOU CLICK START GAME
+        Time.timeScale = 0;
+        Player.Instance.gameObject.SetActive(false);
         StartCoroutine(SpawnTimer());
         StartCoroutine(MainGameTimer());
 
--- Project3/Assets/Scripts/SceneChangeManager.cs
+++ Project3/Assets/Scripts/SceneChangeManager.cs
@@ -6,28 +6,49 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
+    public GameObject startScreen;
+    public GameObject onboardScreen;
+    public GameObject creditsScreen;
+
     public void GoToCredits()
     {
-        SceneManager.LoadScene("Credits");
+        creditsScreen.SetActive(true);
+        onboardScreen.SetActive(false);
+        startScreen.SetActive(false);
+        Debug.Log("Game is going to credits screen");
     }
 
     public void GoToHowToPlay()
     {
-        SceneManager.LoadScene("HowToPlay");
+        creditsScreen.SetActive(false);
+        onboardScreen.SetActive(true);
+        startScreen.SetActive(false);
+        Debug.Log("Game is going to onboarding screen");
     }
 
     public void GoToGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        creditsScreen.SetActive(false);
+        onboardScreen.SetActive(false);
+        startScreen.SetActive(false);
+        // Make player active so bullets work and stuff
+        Player.Instance.gameObject.SetActive(true);
+        // Resume the game
+        Time.timeScale = 1;
+        Debug.Log("Game is starting");
     }
 
     public void GoToStart()
     {
-        SceneManager.LoadScene("StartMenu");
+        creditsScreen.SetActive(false);
+        onboardScreen.SetActive(false);
+        startScreen.SetActive(true);
+        Debug.Log("Game is going to start screen");
     }
 
     public void QuitGame()
     {
-        
+        Debug.Log("Game is exiting");
+        Application.Quit();
     }
 }
