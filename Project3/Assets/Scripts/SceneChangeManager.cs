890247c982ac0ece34324926d4ee95347512c03f
Tyler Garcia <tmg7946@rit.edu>
2022-11-29 00:45:02 -0500

    Did pause screen

--- Project3/Assets/Scripts/SceneChangeManager.cs
+++ Project3/Assets/Scripts/SceneChangeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 using UnityEngine.SceneManagement;
 
@@ -9,6 +10,7 @@ public class SceneChangeManager : MonoBehaviour
     public GameObject startScreen;
     public GameObject onboardScreen;
     public GameObject creditsScreen;
+    public GameObject pauseScreen;
     public Texture2D targetCursorTexture;
 
     public void GoToCredits()
@@ -16,6 +18,7 @@ public class SceneChangeManager : MonoBehaviour
         creditsScreen.SetActive(true);
         onboardScreen.SetActive(false);
         startScreen.SetActive(false);
+        pauseScreen.SetActive(false);
         Debug.Log("Game is going to credits screen");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
@@ -37,6 +40,7 @@ public class SceneChangeManager : MonoBehaviour
         creditsScreen.SetActive(false);
         onboardScreen.SetActive(true);
         startScreen.SetActive(false);
+        pauseScreen.SetActive(false);
         Debug.Log("Game is going to onboarding screen");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
@@ -46,11 +50,12 @@ public class SceneChangeManager : MonoBehaviour
         creditsScreen.SetActive(false);
         onboardScreen.SetActive(false);
         startScreen.SetActive(false);
+        pauseScreen.SetActive(false);
         // Make player active so bullets work and stuff
         Player.Instance.gameObject.SetActive(true);
         // Resume the game
         Time.timeScale = 1;
-        Debug.Log("Game is starting");
+        Debug.Log("Game is starting/resuming");
         Cursor.SetCursor(targetCursorTexture, Vector2.zero, CursorMode.Auto);
     }
 
@@ -59,9 +64,24 @@ public class SceneChangeManager : MonoBehaviour
         creditsScreen.SetActive(false);
         onboardScreen.SetActive(false);
         startScreen.SetActive(true);
+        pauseScreen.SetActive(false);
         Debug.Log("Game is going to start screen");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
+
+    
+    public void OnPause(InputValue value)
+    {
+        pauseScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+        onboardScreen.SetActive(false);
+        startScreen.SetActive(false);
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Time.timeScale = 0;
+        Player.Instance.gameObject.SetActive(false);
+        Debug.Log("Game is pausing");
+    }
+    
 
     public void QuitGame()
     {
